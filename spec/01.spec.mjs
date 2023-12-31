1cc68adae3ee87e7f91cc197811f565fe0e04482
da99 <1551848+da99@users.noreply.github.com>
2023-12-31 15:23:33 -0600

    Started: unit test base.

---
+++ spec/01.spec.mjs
@@ -0,0 +1,6 @@
+
+// import assert from 'assert';
+
+it('should add to numbers', () => {
+  unitjs.value(3 + 5).is(9);
+});
---
+++ spec/index.ts
@@ -0,0 +1,13 @@
+
+import { Hono } from 'hono';
+import { serveStatic } from 'hono/bun';
+
+const app = new Hono()
+app.use('/spec/*', serveStatic({ root: './' }))
+app.use('/tmp/*', serveStatic({ root: './' }))
+app.get('/', (c) => c.text('Hello, Bun!'))
+
+export default {
+  port: 3000,
+  fetch: app.fetch
+}
